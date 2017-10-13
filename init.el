cc7068f1eebf9936a7c4692d0c8b4c2252d44e6f
icylisper <icylisper@gmail.com>
2017-10-12 23:01:44 -0700

    first commit - tangled emacs config

---
+++ init.el
@@ -0,0 +1,7 @@
+(package-initialize)
+
+(defvar my-init-file (expand-file-name "emacs-init.el" user-emacs-directory)
+  "All configurations stored in this file.")
+
+(defvar my-org-file (expand-file-name "emacs-init.org" user-emacs-directory)
+  "All configurations tangled from this file.")
